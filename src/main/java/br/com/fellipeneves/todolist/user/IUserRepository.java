04721719636df27528f939a5fed77b343a9c70f0
fellipesnoww <fellipe2.0e@gmail.com>
2025-08-01 22:29:32 -0300

    chore: Validating username unique and handle with right status code

--- src/main/java/br/com/fellipeneves/todolist/user/IUserRepository.java
+++ src/main/java/br/com/fellipeneves/todolist/user/IUserRepository.java
@@ -2,8 +2,9 @@ package br.com.fellipeneves.todolist.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface  IUserRepository extends JpaRepository<UserModel, UUID> {
-
+    UserModel findByUsername(String username);
 }
