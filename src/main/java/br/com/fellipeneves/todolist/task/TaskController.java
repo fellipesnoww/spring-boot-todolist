2699438bc89688f2c151bbd58242ca43b522fbd5
fellipesnoww <fellipe2.0e@gmail.com>
2025-08-02 20:44:43 -0300

    chore: Validating if user is owner task and if task really exists

--- src/main/java/br/com/fellipeneves/todolist/task/TaskController.java
+++ src/main/java/br/com/fellipeneves/todolist/task/TaskController.java
@@ -52,6 +52,14 @@ public class TaskController {
 
         var task = this.taskRepository.findById(id).orElse(null);
 
+        if(task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Task não encontrada");
+        }
+
+        if(!task.getIdUser().equals(userId)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário não possui permissão para alterar essa task");
+        }
+
         Utils.copyNonNullProperties(taskModel, task);
 
         var updatedTask = this.taskRepository.save(task);
