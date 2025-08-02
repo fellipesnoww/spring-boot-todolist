8aee26d006f119ed173dae681d298a4bc55dc54b
fellipesnoww <fellipe2.0e@gmail.com>
2025-08-02 19:55:48 -0300

    chore: Validating dates in tasks creation

--- src/main/java/br/com/fellipeneves/todolist/task/TaskController.java
+++ src/main/java/br/com/fellipeneves/todolist/task/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @RestController
@@ -23,8 +24,18 @@ public class TaskController {
 
         var userId = request.getAttribute("idUser");
 
+        var currentDate = LocalDate.now();
+        if(currentDate.isAfter(taskModel.getStartAt().toLocalDate()) || currentDate.isAfter(taskModel.getEndAt().toLocalDate())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de inicio e/ou fim, devem ser maior que a data atual");
+        }
+
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de inicio não pode ser maior que a data de término");
+        }
+
+
         taskModel.setIdUser((UUID) userId);
         var newTask = this.taskRepository.save(taskModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newTask);
+        return ResponseEntity.status(HttpStatus.OK).body(newTask);
     }
 }
