97d0343afbddfa91e4c513c84b2b522f74a70953
fellipesnoww <fellipe2.0e@gmail.com>
2025-08-01 22:30:40 -0300

    chore: remove unused import

--- src/main/java/br/com/fellipeneves/todolist/user/IUserRepository.java
+++ src/main/java/br/com/fellipeneves/todolist/user/IUserRepository.java
@@ -2,7 +2,6 @@ package br.com.fellipeneves.todolist.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface  IUserRepository extends JpaRepository<UserModel, UUID> {
